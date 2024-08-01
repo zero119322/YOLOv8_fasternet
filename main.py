62920512b4de624c2dbb033df59d4638ec5b76c7
zero119322 <58361873+zero119322@users.noreply.github.com>
2024-08-01 18:46:23 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
 # Load a model
-model = YOLO("ultralytics/cfg/models/v8/yolov8a.yaml")  # build a new model from scratch
+model = YOLO("ultralytics/cfg/models/v8/yolov8s.yaml")  # build a new model from scratch
 
 
 # Use the model
